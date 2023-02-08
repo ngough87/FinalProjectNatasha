21789769296ffcdc0a1edebf0c9547fbfdc7c9eb
Forrest January <fjanuary9@gmail.com>
2023-02-08 11:01:57 -0600

    Create PreferredWalkType entity and Key

---
+++ JPAPaseo/src/main/java/com/skilldistillery/paseo/entities/PreferredWalkType.java
@@ -0,0 +1,69 @@
+package com.skilldistillery.paseo.entities;
+
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="preferred_walk_type")
+public class PreferredWalkType {
+	@EmbeddedId
+	private PreferredWalkTypeKey id;
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	@MapsId(value="user")
+	private User user;
+	@ManyToOne
+	@JoinColumn(name="walk_type_id")
+	@MapsId(value="walkType")
+	private WalkType walkType;
+	
+	public PreferredWalkType () {}
+
+	public PreferredWalkTypeKey getId() {
+		return id;
+	}
+
+	public void setId(PreferredWalkTypeKey id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public WalkType getWalkType() {
+		return walkType;
+	}
+
+	public void setWalkType(WalkType walkType) {
+		this.walkType = walkType;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PreferredWalkType other = (PreferredWalkType) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
---
+++ JPAPaseo/src/main/java/com/skilldistillery/paseo/entities/PreferredWalkTypeKey.java
@@ -0,0 +1,56 @@
+package com.skilldistillery.paseo.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class PreferredWalkTypeKey {
+	private static final long serialVersionUID = 1L;
+	@Column(name="user_id")
+	private int user;
+	@Column(name="walk_type_id")
+	private int walkType;
+	
+	public PreferredWalkTypeKey () {}
+	
+	public PreferredWalkTypeKey (int user, int walkType) {
+		this.user = user;
+		this.walkType = walkType;
+	}
+
+	public int getUser() {
+		return user;
+	}
+
+	public void setUser(int user) {
+		this.user = user;
+	}
+
+	public int getWalkType() {
+		return walkType;
+	}
+
+	public void setWalkType(int walkType) {
+		this.walkType = walkType;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(user, walkType);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PreferredWalkTypeKey other = (PreferredWalkTypeKey) obj;
+		return user == other.user && walkType == other.walkType;
+	}
+	
+}
