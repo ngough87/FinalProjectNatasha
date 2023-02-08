34e8d00ed367f72f6ecc0d36e4a479b71c8d1e2a
Forrest January <fjanuary9@gmail.com>
2023-02-08 10:34:28 -0600

    Create PreferredGender and key classes

---
+++ JPAPaseo/src/main/java/com/skilldistillery/paseo/entities/PreferredGender.java
@@ -0,0 +1,75 @@
+package com.skilldistillery.paseo.entities;
+
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="preferred_gender")
+public class PreferredGender {
+	@EmbeddedId
+	private PreferredGenderKey id;
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	@MapsId(value="user")
+	private User user;
+	@ManyToOne
+	@JoinColumn(name="gender_id")
+	@MapsId(value="gender")
+	private Gender gender;
+	
+	public PreferredGender() {}
+
+	public PreferredGenderKey getId() {
+		return id;
+	}
+
+	public void setId(PreferredGenderKey id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PreferredGender other = (PreferredGender) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "PreferredGender [id=" + id + ", user=" + user + "]";
+	}
+	
+	
+}
---
+++ JPAPaseo/src/main/java/com/skilldistillery/paseo/entities/PreferredGenderKey.java
@@ -0,0 +1,58 @@
+package com.skilldistillery.paseo.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class PreferredGenderKey {
+	private static final long serialVersionUID = 1L;
+	@Column(name="gender_id")
+	private int gender;
+	@Column(name="user_id")
+	private int user;
+	
+	public PreferredGenderKey () {}
+	
+	public PreferredGenderKey(int gender, int user) {
+		this.gender = gender;
+		this.user = user;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public int getUser() {
+		return user;
+	}
+
+	public void setUser(int user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(gender, user);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PreferredGenderKey other = (PreferredGenderKey) obj;
+		return gender == other.gender && user == other.user;
+	}
+	
+	
+
+}
